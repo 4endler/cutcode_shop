15712822a9f0a662b0ccd04a36acb27354dab9bd
4endler <4endler@gmail.com>
2025-04-11 22:58:40 +0300

    migrations

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Brand;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title' => ucfirst($this->faker->words(2, true)),
+            'brand_id' => Brand::query()->inRandomOrder()->first()->id,
+            'price' => $this->faker->numberBetween(100, 10000),
+        ];
+    }
+}
